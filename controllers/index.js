8de130fa9933889e217db512db90ba8ef91d0d3b
ehochster <eitan.hochster@yale.edu>
2015-11-01 16:05:07 +0000

    unorderedlist

--- controllers/index.js
+++ controllers/index.js
@@ -3,13 +3,20 @@
 /**
  * Controller that renders our index (home) page.
  */
+
+var events = require('../models/events');
+
+
 function index (request, response) {
   var contextData = {
     'title': 'MGT 656',
-    'tagline': 'You are doomed (just kidding).'
+    'tagline': 'You are doomed (just kidding).',
+    'events': events.all
   };
   response.render('index.html', contextData);
 }
+
+
 
 module.exports = {
   index: index
